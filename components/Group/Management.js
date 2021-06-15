2db9f0358fabff266edcc1727b96ea2ff25d1b20
ayowoleadenuga-ukdion <ayowoleadenuga@ukdion.com>
2021-06-15 11:52:53 +0100

    optimized images

--- components/Group/Management.js
+++ components/Group/Management.js
@@ -5,7 +5,7 @@ const Management = () => {
     return (
         <div className="board_of_directors container">
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744060/ukDion%20group/ysnuvrxeoutp0hiocqsj.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744060/ukDion%20group/ysnuvrxeoutp0hiocqsj.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Chinonye V.Ofurum</p>
                     <p className="board_position">Head, Human Resources</p>
@@ -13,7 +13,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744045/ukDion%20group/cde1mutp9xcyfjrcpibs.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744045/ukDion%20group/cde1mutp9xcyfjrcpibs.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Eyinnaya Afiauwa</p>
                     <p className="board_position">Chief Financial Officer</p>
@@ -21,7 +21,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744037/ukDion%20group/ejqs3p74qceqxlw40ex3.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744037/ukDion%20group/ejqs3p74qceqxlw40ex3.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Faith Garba</p>
                     <p className="board_position">Head, Sales</p>
@@ -29,7 +29,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744053/ukDion%20group/yuq5lqbbs1kqyqqigoou.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744053/ukDion%20group/yuq5lqbbs1kqyqqigoou.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Ugo Oyewola</p>
                     <p className="board_position">Chief Investment Officer</p>
@@ -37,7 +37,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744054/ukDion%20group/hdyf16k0jwbty2dzjyyn.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744054/ukDion%20group/hdyf16k0jwbty2dzjyyn.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Adewale E. Ogunlolu</p>
                     <p className="board_position">Chief Risk Officer</p>
@@ -45,7 +45,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744066/ukDion%20group/cycmfyta8r45w0dglehp.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744066/ukDion%20group/cycmfyta8r45w0dglehp.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Florence N. Okoronkwo</p>
                     <p className="board_position">Head, Compliance & Legal</p>
@@ -53,7 +53,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744051/ukDion%20group/lozjnsilgxxr8dt9lhun.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744051/ukDion%20group/lozjnsilgxxr8dt9lhun.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Gloria Agboifoh</p>
                     <p className="board_position">Chief Product Officer</p>
@@ -61,7 +61,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744066/ukDion%20group/ni5p1jo0oesm8sk6qt8j.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744066/ukDion%20group/ni5p1jo0oesm8sk6qt8j.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Nwogo Onyejepu</p>
                     <p className="board_position">Head, Currency Exchange</p>
@@ -69,7 +69,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744055/ukDion%20group/hxywapfkwjnfheeointv.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744055/ukDion%20group/hxywapfkwjnfheeointv.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Mark V. Befe</p>
                     <p className="board_position">Head, Corporate Communications</p>
@@ -77,7 +77,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744062/ukDion%20group/qiczdu3mno48tryhczc9.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744062/ukDion%20group/qiczdu3mno48tryhczc9.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Ezekiel A. Yusuf</p>
                     <p className="board_position">Head, Learning & Development</p>
@@ -85,7 +85,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744029/ukDion%20group/ksm7mulbxue41vdbrhk6.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744029/ukDion%20group/ksm7mulbxue41vdbrhk6.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Timothy O. Atima</p>
                     <p className="board_position">Head, Exchequer</p>
@@ -93,7 +93,7 @@ const Management = () => {
             </div>
 
             <div className="plenty_images">
-                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/v1623744060/ukDion%20group/xqyrfyhnoeca3lsd7kuc.jpg" alt=""/>
+                <Image width="300" height="350" src="https://res.cloudinary.com/dmn7agj1i/image/upload/q_auto:best/v1623744060/ukDion%20group/xqyrfyhnoeca3lsd7kuc.jpg" alt=""/>
                 <div className="text-center">
                     <p className="board_name">Chiamaka O. Opene</p>
                     <p className="board_position">Head, Customer Experience</p>
